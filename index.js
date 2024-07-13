6fa98cd59b38b4e26855b20fd369786ca075a342
Mohammed Fahiz <mohammedfahiz07@gmail.com>
2024-07-14 02:48:57 +0530

    first commit. basic todo list

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+const { Pool } = pkg;
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  // items.push({title: item});
+  try {
+    await pool.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+
+  try {
+    await pool.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await pool.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
